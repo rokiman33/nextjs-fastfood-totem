45d56f5a824c2b1089edbe9dd40dbcd0af265ce5
Lucas <lucasprado.cnpq@natura.net>
2025-02-20 16:55:37 -0300

    feat: add db seed script

---
+++ INFO.md
@@ -0,0 +1,11 @@
+npx creat-next-app@15.1.6
+
+npm install @prisma/client@6.2.1
+npx prisma init
+
+npm install dotenv
+
+npx prisma migrate dev
+
+npm install -D ts-node@10.9.2
+npx prisma db seed (input data in db)
---
+++ prisma/migrations/20250220194638_add_ingredients_array/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - The `ingredients` column on the `Product` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "ingredients",
+ADD COLUMN     "ingredients" TEXT[];
